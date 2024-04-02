0f7f8c1031736097b7c3d801a6892caa9bb92f45
Renato Groffe <renato.groffe@yahoo.com.br>
2024-04-02 18:27:25 -0300

    Commit inicial

---
+++ src/ConsoleAppBase64String/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppBase64String.Models;
+
+public class Item
+{
+    [Required]
+    public string? PlainText { get; set; }
+
+    [Base64String(ErrorMessage = "String base64 invalida")]
+    public string? Encoded { get; set; }
+}
---
+++ src/ConsoleAppBase64String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using ConsoleAppBase64String.Models;
+
+Console.WriteLine("***** Testes com .NET 8 | Base64StringAttribute *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var items = new Item[]
+{
+    new Item() { PlainText = "Primeiro valor", Encoded = "UHJpbWVpcm8gaXRlbQ==" },
+    new Item() { PlainText = "Segundo item", Encoded = "Segundo item" },
+    new Item() { PlainText = "Terceiro item", Encoded = "VGVyY2Vpcm8gaXRlbQ==" },
+    new Item() { PlainText = "Quarto item", Encoded = "4o item" }
+};
+
+foreach (var item in items)
+{
+    Console.WriteLine();
+    Console.WriteLine(JsonSerializer.Serialize(item));
+    var validationResults = new List<ValidationResult>();
+    if (!Validator.TryValidateObject(item, new ValidationContext(item),
+        validationResults, validateAllProperties: true))
+    {
+        Console.WriteLine("Dados invalidos para essa instancia...");
+        foreach (var validationResult in validationResults)
+            Console.WriteLine($"ErrorMessage = {validationResult.ErrorMessage}");
+    }
+}
